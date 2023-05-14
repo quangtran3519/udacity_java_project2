0d987f3e79a398096e3d7acafb00dd90f47398cb
Quang <quangtran3519@gmail.com>
2023-05-15 00:25:00 +0700

    first commit

---
+++ eureka/src/test/java/com/udactiy/eureka/EurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.udactiy.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+
+
+
+}
---
+++ vehicles-api/src/main/java/com/udacity/vehicles/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.udacity.vehicles.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.service.ApiInfo;
+
+import java.util.Collections;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "CAR REST API",
+                "This API  System for a Car Website",
+                "1.0",
+                "",
+                null,
+                "License of API", "", Collections.emptyList());
+    }
+
+}
